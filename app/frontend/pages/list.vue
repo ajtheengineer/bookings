c461a2968919c0d030c6069f44c94c2e393b38d9
ajtheengineer <ajtheengineer@gmail.com>
2023-03-12 08:06:03 -0700

    Add fetching a list of places from the server
    Also I go through use of console.log.

--- app/frontend/pages/list.vue
+++ app/frontend/pages/list.vue
@@ -1,3 +1,31 @@
 <template>
   List of places
+  <div>
+    {{ placesFromServer }}
+  </div>
 </template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  // When the page loads, I haven't hit the server yet for the places.
+  // So there are no places to show right now.
+  const placesFromServer = ref([]);
+  
+  // On page load, I want to ask the server to give me a list of places in json format.
+  // Once i get the data in json format, I want to iterate through the list of places
+  // and render it on the screen.
+  function fetchPlaces() {
+    fetch("/api/places")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        placesFromServer.value = data;
+      });
+  }
+
+  // Wait 3 seconds before fetching places from the backend
+  setTimeout(function() {
+    fetchPlaces()
+  }, 3000)
+</script>
