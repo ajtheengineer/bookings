2fa6ab3e694349000122715bd7b464246381c8d6
ajtheengineer <ajtheengineer@gmail.com>
2023-04-24 10:14:51 -0700

    Adding flake.nix to see if it changes things

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "booking-tutorial";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs-ruby.url = "github:bobvanderlinden/nixpkgs-ruby";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, nixpkgs-ruby, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [
+            nixpkgs-ruby.packages."${system}"."ruby-3.2.1"
+            pkgs.postgresql
+            pkgs.chromedriver
+            pkgs.yarn
+            pkgs.overmind
+            pkgs.nodejs
+            pkgs.heroku
+
+            # necessary for nokogiri gem build
+            pkgs.zlib
+            pkgs.lzma
+          ];
+        };
+      });
+}
